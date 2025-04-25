257580f1ff59b05992a7891eb74abec67a3a9240
Alnoman Robin <robinalnoman@gmail.com>
2025-04-25 12:23:19 +0600

    Comment

---
+++ 05.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //This is a single line comment
+    cout<< "Comment";
+    /* This is
+    multiple Line
+    comment */
+    return 0;
+}
