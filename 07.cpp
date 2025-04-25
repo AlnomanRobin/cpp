fdcf3f5fea17fc8415a1c8c9e4cd192ca012a0d1
Alnoman Robin <robinalnoman@gmail.com>
2025-04-26 00:10:25 +0600

    Declare Multiple Variables

---
+++ 07.cpp
@@ -0,0 +1,12 @@
+//Declare Multiple Variables
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=5,y=6,z=50;
+    cout<< x+y+z<<endl;
+    int a,b,c;
+    a=b=c=50;
+    cout<<a+b+c;
+    return 0;
+}
