9f110c096befaed0a89e0e595ac8d4e493377217
Alnoman Robin <robinalnoman@gmail.com>
2025-04-25 12:18:25 +0600

    New lines

---
+++ 04.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//New Lines
+int main(){
+    cout<< "Hello world! \n";
+    cout<< "I am learning C++" << "\n\n";
+    cout<< "My name is Robin" << endl; // \n=endl
+    cout<< "My ang!\\" << "is :25" <<endl;
+    cout<< "They call him \"Johnny\".";
+    return 0;
+}
