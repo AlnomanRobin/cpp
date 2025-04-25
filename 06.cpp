ab9afbd906d8e451eb0459aa19a772d199a2e4e5
Alnoman Robin <robinalnoman@gmail.com>
2025-04-26 00:09:47 +0600

    Variables

---
+++ 06.cpp
@@ -0,0 +1,29 @@
+#include<iostream> //Variables
+using namespace std;
+
+int main(){
+    int num=25;
+    cout<< num<<endl;
+
+    int number=10;
+    number=20;
+    cout<< number<< "\n\n";
+
+    double A=12.25;
+    cout<< A<< endl;
+    char my_Letter='D';
+    cout<< my_Letter<<endl;
+    string my_Text="Hello Robin";
+    cout<< my_Text<<endl;
+    bool my_Boolean=true;
+    cout<< my_Boolean<<"\n\n";
+
+    int myAge=18;
+    cout<< "I am "<< myAge<< "years old."<<endl;
+
+    int x=20;
+    int y=15;
+    int sum=x+y;
+    cout<< sum;
+    return 0;
+}
