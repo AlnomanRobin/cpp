e37585c0cb883946a09d741c2d5ede0a894d75cb
Alnoman Robin <robinalnoman@gmail.com>
2025-04-27 02:25:29 +0600

    Variables Examples

---
+++ 09.cpp
@@ -0,0 +1,14 @@
+//Constants
+#include <iostream>
+using namespace std;
+
+int main() {
+  const int myNum = 15;
+  myNum = 10;
+  cout << myNum;
+
+  const int minutesPerHour;
+  minutesPerHour = 60;
+  cout << minutesPerHour;
+  return 0;
+}
