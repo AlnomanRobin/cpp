ae1adf641eca35afb5e91c6a91394d9191a3088b
Alnoman Robin <robinalnoman@gmail.com>
2025-04-27 02:25:02 +0600

    Identifiers

---
+++ 08.cpp
@@ -0,0 +1,12 @@
+// Identifiers
+#include<iostream>
+using namespace std;
+
+int main(){
+    int minutesPerHour = 60;//Good
+    cout<< minutesPerHour<<end;
+
+    int m=70;
+    cout<< m;
+    return 0;
+}
