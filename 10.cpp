528cc3b5d533b0834a42f9bde0ae96d8692e4d4f
Alnoman Robin <robinalnoman@gmail.com>
2025-05-08 00:20:26 +0600

    User Input

---
+++ 10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+//#include<conio.h>
+using namespace std;
+int main()
+{
+    int num1=10,num2=20;
+    cout<< "Number1= "<< num1<<endl<< "\"Number2\" = "<< num2<<endl;
+
+    char ch='s';
+    cout<< ch<<endl;
+
+    char name[14]= "Alnoman Robin";//string
+    cout<<"My name is = "<<name<<endl;
+
+
+
+    double a=10.5,b=20.548888856;
+    cout<< a<<endl<< b;
+
+    return 0;//getvh();
+}
