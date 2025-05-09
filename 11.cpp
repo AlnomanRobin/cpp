71070c14fd25bf9763ec6f15c6b1a151990684c4
Alnoman Robin <robinalnoman@gmail.com>
2025-05-09 22:02:53 +0600

    User Input

---
+++ 11.cpp
@@ -0,0 +1,21 @@
+// User Input
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout<< "Type favorite number : ";
+    cin>> x;
+    cout<< "Your name is : "<<x;
+
+    cout<< "\n\n";
+
+    int A,B;
+    cout<< "Right first number : ";
+    cin>> A;
+    cout<< "Right second number : ";
+    cin>> B;
+    int sum=A+B;
+    cout<< "The SUM is : "<<sum;
+    return 0;
+}
