12637fa2c82a84090be868430812bf31c8649ff7
Alnoman Robin <robinalnoman@gmail.com>
2025-05-10 03:23:42 +0600

    Numeric Data Types

---
+++ 13.cpp
@@ -0,0 +1,21 @@
+//Numeric Data Types
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1=1000;
+    cout<< num1<< "\n";
+
+    float num2=5.75;
+    cout<< num2<< "\n";
+
+    double num3=19.99;
+    cout<< num3<< "\n";
+
+    float f1 = 35e3;
+    double d1 = 12E4;
+    cout << f1 << "\n";
+    cout << d1;
+    return 0;
+}
+
