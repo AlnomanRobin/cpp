2b6ddfc73414a27bc87bad351cd43d82ddd78a47
Alnoman Robin <robinalnoman@gmail.com>
2025-05-10 03:24:32 +0600

    Boolean Data Types

---
+++ 14.cpp
@@ -0,0 +1,11 @@
+//Boolean Data Types
+#include<iostream>
+using namespace std;
+
+int main(){
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+    cout << isCodingFun<< endl;  // Outputs 1 (true)
+    cout << isFishTasty;  // Outputs 0 (false)
+    return 0;
+}
