887e88d9a5c5aa6ee768834565ef4416f794f07a
Alnoman Robin <robinalnoman@gmail.com>
2025-05-11 15:23:30 +0600

    Character Data Types

---
+++ 15.cpp
@@ -0,0 +1,14 @@
+//Character Data Types
+#include<iostream>
+using namespace std;
+
+int main(){
+    char myGrade = 'B';
+    cout << myGrade<< "\n";
+
+    char a = 65, b = 66, c = 67;
+    cout << a<<endl;
+    cout << b<<endl;
+    cout << c<<endl;
+    return 0;
+}
